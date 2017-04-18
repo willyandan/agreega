9b1ce3e04927fe8386c97b8ffce4a9e9c32c2a91
Walnei Antunes <Walnei Antunes>
2017-04-17 21:42:38 -0300

    commit inical

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,152 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'as' => 'admin'], function() {
+	//LOGIN OU MENU
+    Route::get('/', 'Admin@index')->name('index');
+    //PROFESSORES
+    Route::group(['prefix' => 'professor', 'as' => 'professor'], function() {
+        //LISTA DE PROFESSORES
+        Route::get('/', function() {
+	        
+	    });
+
+	    //EDITAR PROFESSOR 
+	    Route::get('editar/{id}', function() {
+	        
+	    });
+	    Route::post('atualizar', function() {
+	        
+	    });
+
+	    //CADASTRAR
+	    Route::get('cadastrar', function() {
+	        
+	    });
+	    Route::post('salvar', function() {
+	        
+	    });
+
+	    //DELETAR
+	    Route::delete('deletar', function() {
+	        
+	    });
+    });
+
+    //ALUNOS
+    Route::group(['prefix' => 'aluno', 'as' => 'aluno'], function() {
+        //LISTA DE ALUNOS
+    	Route::get('/', function() {
+    	    
+    	});
+
+        //CADASTRAR ALUNO
+    	Route::get('cadastrar', function() {
+    	    //
+    	});
+    	Route::post('salvar', function() {
+    	    //
+    	});
+
+        //EDITAR ALUNO
+    	Route::get('editar/{id}', function() {
+    	    //
+    	});
+    	Route::post('atualizar', function() {
+    	    //
+    	});
+
+        //DELETAR ALUNO
+        Route::delete('deletar', function() {
+            //
+        });
+    });
+
+    //MATERIAS
+    Route::group(['prefix' => 'materia', 'as' => 'materia'], function() {
+        //LISTA DE MATERIA
+    	Route::get('/', function() {
+    	    
+    	});
+
+        //CADASTRAR MATERIA
+    	Route::get('cadastrar', function() {
+    	    //
+    	});
+    	Route::post('salvar', function() {
+    	    //
+    	});
+
+        //EDITAR MATERIA
+    	Route::get('editar/{id}', function() {
+    	    //
+    	});
+    	Route::post('atualizar', function() {
+    	    //
+    	});
+    	
+        //DELETAR MATERIA
+        Route::delete('deletar', function() {
+            //
+        });
+    });
+
+    //CLASSES
+    Route::group(['prefix' => 'classe', 'as' => 'classe'], function() {
+        //LISTA DE CLASSE
+    	Route::get('/', function() {
+    	    
+    	});
+
+        //CADASTRAR CLASSE
+    	Route::get('cadastrar', function() {
+    	    //
+    	});
+    	Route::post('salvar', function() {
+    	    //
+    	});
+
+        //EDITAR CLASSE
+    	Route::get('editar/{id}', function() {
+    	    //
+    	});
+    	Route::post('atualizar', function() {
+    	    //
+    	});
+    	
+        //DELETAR CLASSE
+        Route::delete('deletar', function() {
+            //
+        });
+    });
+
+    //HORARIOS
+    Route::group(['prefix' => 'horario', 'as' => 'horario'], function() {
+        //LISTA DE HORARIO
+    	Route::get('/', function() {
+    	    
+    	});
+
+        //CADASTRAR HORARIO
+    	Route::get('cadastrar', function() {
+    	    //
+    	});
+    	Route::post('salvar', function() {
+    	    //
+    	});
+
+        //EDITAR HORARIO
+    	Route::get('editar/{id}', function() {
+    	    //
+    	});
+    	Route::post('atualizar', function() {
+    	    //
+    	});
+    	
+        //DELETAR HORARIO
+        Route::delete('deletar', function() {
+            //
+        });
+    });
+});
